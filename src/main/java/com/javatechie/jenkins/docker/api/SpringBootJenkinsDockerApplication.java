4b1023d6b89cf21420f638d648d1f144c38ea481
Harsh Kumar <harsh.kumar@testingXperts.com>
2022-04-27 13:42:38 +0530

    second co mit

--- src/main/java/com/javatechie/jenkins/docker/api/SpringBootJenkinsDockerApplication.java
+++ src/main/java/com/javatechie/jenkins/docker/api/SpringBootJenkinsDockerApplication.java
@@ -2,10 +2,17 @@ package com.javatechie.jenkins.docker.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootJenkinsDockerApplication {
 
+	@GetMapping("/message")
+	public String message() {
+		return "jenkins docker integration";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsDockerApplication.class, args);
 	}
